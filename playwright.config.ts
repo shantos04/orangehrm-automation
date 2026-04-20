4a40a815009613f41a30c3a14c9e5d7ea625db0f
shantos04 <luongtuanvy04@gmail.com>
2026-04-20 09:59:11 +0700

    Update Playwright configuration to use environment variable for base URL

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-
+    baseURL: process.env.BASE_URL,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -43,15 +43,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+//     {
+//       name: 'firefox',
+//       use: { ...devices['Desktop Firefox'] },
+//     },
+//
+//     {
+//       name: 'webkit',
+//       use: { ...devices['Desktop Safari'] },
+//     },
 
     /* Test against mobile viewports. */
     // {
