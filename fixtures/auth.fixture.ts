5b0eb0e6c668d0a3041c80a9ac805e609b24c18b
Luong Vy <luongtuanvy04@gmail.com>
2026-05-25 11:50:33 +0700

    feat: add Auth fixture to support API testing with Playwright

---
+++ fixtures/auth.fixture.ts
@@ -0,0 +1,15 @@
+import { test as base } from '@playwright/test';
+import { AuthAPI } from '../app/api-helpers/auth.api';
+
+type AuthFixtures = {
+    authAPI: AuthAPI;
+};
+
+export const test = base.extend<AuthFixtures>({
+    authAPI: async ({ request, page }, use) => {
+        const authAPI = new AuthAPI(request, page);
+        await use(authAPI);
+    },
+});
+
+export { expect } from '@playwright/test';
